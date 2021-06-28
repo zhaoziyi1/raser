9fce75941b402acefcc1f5569ed56ed27a5ec4cd
Xin Shi <Xin.Shi@outlook.com>
2021-06-28 13:30:34 +0800

    add geant4 build and setup

---
+++ geant4_setup.sh
@@ -0,0 +1,9 @@
+# Setup file for GEANT4 as user inside raser 
+# Author SHI Xin <shixin@ihep.ac.cn>
+# Created [2021-06-28 Mon 13:05]
+
+source $HOME/geant4/install/bin/geant4.sh 
+export PYTHONPATH=$PYTHONPATH:$HOME/geant4/install/lib/python3.8/site-packages
+export GEANT4_INSTALL=$HOME/geant4/install
+
+
